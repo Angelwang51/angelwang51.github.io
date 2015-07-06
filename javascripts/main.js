89cb90897c1e5430590962cfe591ac06fadca524
Yuting Wang <angelwang51@gmail.com>
2015-07-05 23:36:24 -0500

    built responsive page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+	$(".btn").click(function(){
+		$.post("https://torrid-heat-2114.firebaseio.com/contactMe.json",
+			JSON.stringify({
+				name:$(".contact-name").val(),
+				email:$(".contact-email").val(),
+				subject:$(".contact-subject").val(),
+				message:$(".contact-message").val(),
+			})
+			
+			);
+		$(".contact-name").val("");
+			$(".contact-email").val("");
+			$(".contact-subject").val("");
+			$(".contact-message").val("");
+	});
+	function blinker() {
+    $('.focus-code').fadeOut(500);
+    $('.focus-code').fadeIn(500);
+}
+
+    setInterval(blinker, 1000);
+});
